3e81bfa91c5c25bc32fbb1f2db9d6443d6222588
Aditya Halder <144584943+AdityaHalder@users.noreply.github.com>
2025-08-22 17:41:59 +0530

    Update Erixter.py

--- Erixter.py
+++ Erixter.py
@@ -114,9 +114,7 @@ async def lifespan(app: FastAPI):
 
     # --- Shutdown ---
     print("Shutting down...")
-    bot.dispatcher.groups.clear()
     await bot.stop()   # this is enough
-    await asyncio.sleep(0)
     print("Bot stopped")
     
 

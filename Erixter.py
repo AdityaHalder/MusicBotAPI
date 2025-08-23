4b089948d597a77faad072ba347c105aa9f0c53f
Aditya Halder <144584943+AdityaHalder@users.noreply.github.com>
2025-08-23 22:41:24 +0530

    Update Erixter.py

--- Erixter.py
+++ Erixter.py
@@ -188,13 +188,13 @@ async def search_videos(
 
     # 7. Send to Telegram
     if video:
-        tg_msg = await bot.send_document(
+        tg_msg = await bot.send_video(
             chat_id=CHANNEL_ID,
             document=filepath,
-            # duration=duration_seconds,
+            duration=duration_seconds,
             file_name=file_name,
         )
-        file_id = tg_msg.video.file_id or tg_msg.document.file_id
+        file_id = tg_msg.video.file_id
     else:
         tg_msg = await bot.send_audio(
             chat_id=CHANNEL_ID,
